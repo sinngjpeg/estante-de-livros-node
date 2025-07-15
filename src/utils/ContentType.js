519aa8a8bd0bf69385f543f6574fc18bc62eb4a8
Ingrid Silva <ingridsilvatec@outlook.com>
2025-07-14 21:21:14 -0300

    add utils

---
+++ src/utils/ContentType.js
@@ -0,0 +1,9 @@
+
+const ContentType = Object.freeze({
+    JSON: "application/json",
+    MULTIPART: "multipart/form-data",
+    URL_ENCODED: "application/x-www-form-urlencoded",
+});
+
+
+module.exports = { ContentType };
---
+++ src/utils/HttpMethods.js
@@ -0,0 +1,9 @@
+const HttpMethods = Object.freeze({
+    GET: "GET",
+    POST: "POST",
+    PUT: "PUT",
+    DELETE: "DELETE"
+});
+
+
+module.exports = { HttpMethods };
