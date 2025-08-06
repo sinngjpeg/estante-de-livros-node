99b7ae08d3fc7d7124055635938d7ffce8703511
Ingrid Silva <ingridsilvatec@outlook.com>
2025-08-05 23:05:26 -0300

    add cors configs

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ const router = require('./routers/Routers.js');
 const swaggerUi = require('swagger-ui-express'); // importar swaggerUi aqui
 const YAML = require('yamljs');
 const path = require('path');
-
+const cors = require('cors');
 
 const swaggerDocument = YAML.load(path.join(__dirname, '..', 'docs', 'swagger.yaml'));
 
 
 function createServer() {
     const app = express();
+    app.use(cors({
+        origin: 'http://localhost:5173', // libera só seu frontend
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // métodos permitidos
+        credentials: true, // se precisar enviar cookies
+    }));
     app.use(express.json());
     app.use("/api", router)
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
     return app;
 }
 
